33a7181e3d22d8622754ef9dc018aeb82072f105
Anas Ibn Belal <yorder07@gmail.com>
2024-08-03 11:19:48 +0600

    chore: Refactor server.js to improve CORS configuration

--- server.js
+++ server.js
@@ -8,11 +8,28 @@ const app = express();
 require("dotenv").config();
 
 // Configure CORS options
-app.use(
-  cors({
-    origin: "https://videoarchive.vercel.app", // Your frontend origin
-  })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://videoarchive.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Enable preflight for all routes
 
 // Multer setup
 const storage = multer.memoryStorage(); // Store file in memory
